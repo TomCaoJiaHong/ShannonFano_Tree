55620fb1af455fe66ae1d59ee4cef3ba8ece50f8
Jiahong Cao <52592144+TomCaoJiaHong@users.noreply.github.com>
2023-04-24 09:23:33 -0300

    Create genTree_Shannon.m

---
+++ genTree_Shannon.m
@@ -0,0 +1,6 @@
+function tree = genTree_Shannon(stringOrFreqs)
+    % GENTREE Generate a Huffman tree either from a given list of
+    %   frequencies or from string used to compute frequencies.
+    
+
+end
