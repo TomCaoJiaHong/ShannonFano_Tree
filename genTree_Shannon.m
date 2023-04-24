6c01c6d644c8c287f77074881a2f948136deea41
Jiahong Cao <52592144+TomCaoJiaHong@users.noreply.github.com>
2023-04-24 09:23:47 -0300

    Update genTree_Shannon.m

--- genTree_Shannon.m
+++ genTree_Shannon.m
@@ -2,5 +2,14 @@ function tree = genTree_Shannon(stringOrFreqs)
     % GENTREE Generate a Huffman tree either from a given list of
     %   frequencies or from string used to compute frequencies.
     
-
+    if iscell(stringOrFreqs)        % frequencies given
+        syms = stringOrFreqs(1, :)';
+        freqs = [stringOrFreqs{2, :}]';
+    else                            % frequencies not given
+        syms = cellstr(char(32:126)');
+        syms{1} = ' ';
+        count = @(c) sum(ismember(stringOrFreqs, c));
+        freqs = cellfun(count, syms);
+    end
+    
 end
