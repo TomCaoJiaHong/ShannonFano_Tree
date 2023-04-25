f367b92fccffb29384335c8206416bf64c0859a1
Jiahong Cao <52592144+TomCaoJiaHong@users.noreply.github.com>
2023-04-25 08:54:25 -0300

    Update encode.m

--- encode.m
+++ encode.m
@@ -1,5 +1,12 @@
 function code = encode(string, tree)
 
+    function code = encSym(sym, tree)
+    end
+
+    encodeSymbol = @(c) encSym(c, tree);
+
+    code = arrayfun(encodeSymbol, string, 'UniformOutput', false);
+    code = char(48+uint8([code{:}]));
 
 end
 
