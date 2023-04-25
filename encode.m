578a7a19ea31332064c5ba3d41a555c72c5959d3
Jiahong Cao <52592144+TomCaoJiaHong@users.noreply.github.com>
2023-04-25 08:54:31 -0300

    Update encode.m

--- encode.m
+++ encode.m
@@ -1,6 +1,16 @@
 function code = encode(string, tree)
 
     function code = encSym(sym, tree)
+        if length(tree{1})==1
+            code = [];
+        else
+            left = tree{2};
+            if sum(ismember(left{1}, sym))
+                code = [0 encSym(sym, left)];
+            else
+                code = [1 encSym(sym, tree{3})];
+            end
+        end
     end
 
     encodeSymbol = @(c) encSym(c, tree);
