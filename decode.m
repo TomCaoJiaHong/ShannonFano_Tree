c873ca7e15efb65518961d8c0eec463c4c719ed0
Jiahong Cao <52592144+TomCaoJiaHong@users.noreply.github.com>
2023-05-11 14:42:12 -0300

    Update decode.m

--- decode.m
+++ decode.m
@@ -4,6 +4,11 @@ function string = decode(code, tree)
         if length(tree{1})==1
             sym = tree{1};
             remainingCode = code;
+        elseif code(1)=='0'
+            [sym, remainingCode] = dec(code(2:end), tree{2});
+        else
+            [sym, remainingCode] = dec(code(2:end), tree{3});
+        end
     end
 
 
