7f3d287ccb2f2051df064dd9f9eb17980e1ec485
Jiahong Cao <52592144+TomCaoJiaHong@users.noreply.github.com>
2023-05-13 09:52:08 -0300

    Update drawTree.m

--- drawTree.m
+++ drawTree.m
@@ -1,4 +1,9 @@
 %draw the binary tree
 function [] = drawTree(tree, name)
+    %store the relationship and symbol names
+    father = zeros(1,2*length(tree{1,1}));
+    ttext = cell(1,2*length(tree{1,1}));
+    %keep track number of nodes
+    num = 0;
     
 end
