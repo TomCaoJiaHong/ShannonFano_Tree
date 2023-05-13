c9e2be4abfd987b67838611d6cb192df318f44f9
Jiahong Cao <52592144+TomCaoJiaHong@users.noreply.github.com>
2023-05-13 09:52:25 -0300

    Update drawTree.m

--- drawTree.m
+++ drawTree.m
@@ -6,4 +6,16 @@ function [] = drawTree(tree, name)
     %keep track number of nodes
     num = 0;
     
+    %A recursive function, read a node, then add the node and it's childs
+    %to father and ttext
+    function [numout] = readtree(tree)
+        %check if the node is leaf node
+        if size(tree{1,2},2)==0
+            %if leaf node, increase the node number and return
+            num = num+1;
+            numout = num;
+        
+    end
+    
+    
 end
